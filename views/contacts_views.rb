80519671202c701c4b12bdbdf160285f9316d5f9
JackMarx <seriousfools@gmail.com>
2018-01-28 00:48:59 -0600

    mvc

---
+++ views/contacts_views.rb
@@ -0,0 +1,20 @@
+module ContactsViews
+  def contacts_index_view(contacts)
+    contacts.each do |contact|
+      puts "=" * 70
+      contacts_show_view(contact)
+    end
+    puts "=" * 70
+  end
+
+  def contacts_show_view(contact)
+    puts
+    puts "#{contact.full_name} - last updated: #{contact.updated_at}"
+    puts "------------------------------"
+    puts "Phone: #{contact.phone_number}"
+    puts "Email: #{contact.email}"
+    puts
+    puts contact.bio
+    puts
+  end
+end
